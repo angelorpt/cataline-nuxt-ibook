60748523ee6fd9900d41de249de6f93ef40ed6f6
Angelo Rafael <angelorpt@gmail.com>
2021-12-01 21:24:58 -0400

    add | Bosón Container e Config autoimport

---
+++ layouts/ibook.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="ibook-layout">
+    <!-- header -->
+    <Container>
+      <Nuxt />
+    </Container>
+    <!-- footer -->
+  </div>
+</template>
+
+<style scoped>
+.ibook-layout {
+  margin-top: 4.313rem;
+  display: grid;
+  grid-gap: 6.75rem;
+  .container {
+    padding-bottom: 6.75rem;
+  }
+  .footer {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+  }
+}
+</style>
